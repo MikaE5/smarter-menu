fa041badd7c16182721f59e88ae97f12396f1bd5
MikaE5 <mika.eckhardt@gmx.de>
2021-05-05 20:33:52 +0200

    redesign menu item

--- digital-menu-web/src/components/MenuItem.svelte
+++ digital-menu-web/src/components/MenuItem.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-  import { Card, CardText, CardTitle, CardBody, CardHeader } from 'sveltestrap';
+  import {
+    Card,
+    CardText,
+    CardTitle,
+    CardHeader,
+    Row,
+    Col,
+    Modal,
+    ModalHeader,
+    ModalBody,
+  } from 'sveltestrap';
   import { addToBookmarks } from '../bookmarks/bookmarks.util';
   import { getCategoryImagePath } from '../util/image.util';
   import { getPriceString } from '../util/price.util';
@@ -16,17 +26,58 @@
   $: {
     priceString = getPriceString(menuItem.price.amount, menuItem.price.unit);
   }
+
+  let open = false;
+  const toggle = () => (open = !open);
 </script>
 
-<Card color="light">
-  <CardHeader>
-    <div class="d-flex justify-content-between">
-      <CardTitle>{menuItem.name}</CardTitle>
-      <ClickIcon click={() => addToBookmarks(menuItem.id)} icon="plus-circle" />
-    </div>
-    <CardText>{priceString}</CardText>
-  </CardHeader>
-  <CardBody>
-    <img src={imgSrc} class="img-fluid category-image" alt="" />
-  </CardBody>
-</Card>
+<div>
+  <Card color="light">
+    <CardHeader>
+      <div class="d-flex justify-content-between">
+        <img
+          on:click={toggle}
+          src={imgSrc}
+          alt=""
+          class="menu-item-image mr-2"
+        />
+        <Row cols={1}>
+          <Col>
+            <div class="d-flex justify-content-between flex-grow-1">
+              <CardTitle>{menuItem.name}</CardTitle>
+              <ClickIcon
+                click={() => addToBookmarks(menuItem.id)}
+                icon="plus-circle"
+              />
+            </div>
+          </Col>
+          <Col><CardText>{priceString}</CardText></Col>
+        </Row>
+      </div>
+    </CardHeader>
+    <!--  <CardBody>
+      <img src={imgSrc} class="img-fluid category-image" alt="" />
+    </CardBody> -->
+  </Card>
+
+  <Modal isOpen={open} {toggle}>
+    <ModalHeader {toggle}>{menuItem.name}</ModalHeader>
+    <ModalBody>
+      <img src={imgSrc} alt="" class="modal-image" />
+    </ModalBody>
+  </Modal>
+</div>
+
+<style>
+  .menu-item-image {
+    object-fit: cover;
+    width: 20vw;
+    height: 20vw;
+    max-width: 20vw;
+    max-height: 20vw;
+  }
+
+  .modal-image {
+    max-width: 100%;
+  }
+</style>
