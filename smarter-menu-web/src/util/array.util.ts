e5e915333f770f284a4e30d6f18c9804b12ecbae
MikaE5 <mika.eckhardt@gmx.de>
2021-07-20 19:52:27 +0200

    fix sorting of number values

--- smarter-menu-web/src/util/array.util.ts
+++ smarter-menu-web/src/util/array.util.ts
@@ -13,8 +13,8 @@ export const sortByKeys = (arr: object[], keys: string[]): object[] => {
 const compareValues = (a: any, b: any): number => {
   if (a === undefined && b === undefined) return 0;
   if (a === undefined || b === undefined) return a === undefined ? 1 : -1;
-  if (typeof a === 'string' && typeof b === 'string') {
+  if (typeof a === 'string' && typeof b === 'string')
     return a === b ? 0 : a < b ? -1 : 1;
-  }
+  if (typeof a === 'number' && typeof b === 'number') return a < b ? -1 : 1;
   return 0;
 };
